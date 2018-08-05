15d15ae8bee1a02fbd671e21d0febf07cbd15550
Faris Nasution <funyako.funyao156@gmail.com>
2018-08-06 06:10:05 +0700

    Add simple validation

--- src/solaire/boot.clj
+++ src/solaire/boot.clj
@@ -1,6 +1,5 @@
 (ns solaire.boot
   (:require
-   [clojure.pprint :refer [pprint]]
    [boot.core :as bt]
    [boot.util :as btu]
    [clojure.tools.namespace.dir :as nsdir]
@@ -30,18 +29,25 @@
              (seq)
              (boolean))))))
 
+(defn- validate-option
+  [{:keys [paths regexes]}]
+  (when (and (true? paths) (true? regexes))
+    (btu/fail "Cannot specify both --paths and --regexes to true")
+    (throw (ex-info "Incorrect option" {}))))
+
 (bt/deftask system
-  [s sys   VAL  sym   "Symbol of the system. Should point to a non-arity fn"
-   f files VALS [str] "Files to be watched for restarting system. Name-based."
-   p paths      bool  "Treat `files` as a path. Pick between this and `regexes`"
-   r regexes    bool  "Treat `files` as a regex. Pick between this and `paths`"]
+  [s system VAL  sym   "Symbol of the system. Should point to a non-arity fn"
+   f files  VALS [str] "Files to be watched for restarting system. Name-based."
+   p paths       bool  "`files` as path. Pick between this and `regexes`"
+   r regexes     bool  "`files` as regex. Pick between this and `paths`"]
+  (validate-option *opts*)
   (let [prev-fileset_ (atom nil)
         dirs          (into [] (bt/get-env :directories))
         tracker_      (atom (nsdir/scan-dirs (nstrk/tracker) dirs))
-        init-system   (if (some? sys)
+        init-system   (if (some? system)
                         (delay
                          (btu/info "Starting system...\n")
-                         (rpl/setup! sys)
+                         (rpl/setup! system)
                          (rpl/boot!))
                         (delay
                          (btu/info "No system is supplied."
@@ -50,7 +56,7 @@
       (fn [fileset]
         (when (realized? init-system)
           (swap! tracker_ nsdir/scan-dirs)
-          (rpl/reboot! @tracker_ (restart-system? sys
+          (rpl/reboot! @tracker_ (restart-system? system
                                                   @prev-fileset_
                                                   fileset
                                                   files
